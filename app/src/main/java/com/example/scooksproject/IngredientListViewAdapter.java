c41d6243d22fc36916e4df277588ea417713ca3e
nadavq <nadavholtzman@gmail.com>
2022-03-29 03:38:50 +0300

    first progress commit

---
+++ app/src/main/java/com/example/scooksproject/IngredientListViewAdapter.java
@@ -0,0 +1,56 @@
+package com.example.scooksproject;
+
+import android.app.Activity;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.NumberPicker;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+public class IngredientListViewAdapter extends ArrayAdapter<String> {
+
+    private Context context;
+    private ArrayList<String> items;
+
+    public IngredientListViewAdapter(@NonNull Context context, ArrayList<String> items) {
+        super(context, R.layout.ingredient_list_item, items);
+        this.context = context;
+        this.items = items;
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        if(convertView == null) {
+            LayoutInflater layoutInflater = (LayoutInflater) context.getSystemService(Activity.LAYOUT_INFLATER_SERVICE);
+            convertView = layoutInflater.inflate(R.layout.ingredient_list_item, null);
+
+            TextView ingredientName = convertView.findViewById(R.id.nameIngredient);
+            Button amountBtn = convertView.findViewById(R.id.amountIngredient);
+            NumberPicker numberPicker = convertView.findViewById(R.id.numberPickerIngredient);
+            ImageView removeIngredient = convertView.findViewById(R.id.removeIngredient);
+
+            numberPicker.setMinValue(1);
+            numberPicker.setMaxValue(10);
+            removeIngredient.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    IngredientsActivity.removeItem(position);
+                }
+            });
+            ingredientName.setText(items.get(position));
+        }
+        return convertView;
+    }
+}
--- app/src/main/java/com/example/scooksproject/IngredientsActivity.java
+++ app/src/main/java/com/example/scooksproject/IngredientsActivity.java
@@ -4,23 +4,51 @@ import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ListView;
 import android.widget.PopupMenu;
+
+import java.util.ArrayList;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class IngredientsActivity extends AppCompatActivity implements PopupMenu.OnMenuItemClickListener {
 
-    private Button amountTypeBtn7;
+    private ListView listView;
+    private static IngredientListViewAdapter adapter;
+    private static ArrayList<String> items;
+    private static int lastBtnClicked;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.ingredients);
+        setContentView(R.layout.ingredients_activity);
+        initComponents();
     }
 
     private void initComponents(){
-        amountTypeBtn7 = findViewById(R.id.amountTypeBtn7);
+        listView = findViewById(R.id.ingredientsListView);
+        items = new ArrayList<>();
+        adapter = new IngredientListViewAdapter(this, items);
+        listView.setAdapter(adapter);
+        addItem("קמח");
+        addItem("סוכר");
+        addItem("לחם");
+        addItem("רום");
+    }
+
+    public static void setLastBtnClicked(int lastBtnClicked) {
+        IngredientsActivity.lastBtnClicked = lastBtnClicked;
+    }
+
+    public static void addItem(String str){
+        items.add(str);
+        adapter.notifyDataSetChanged();
+    }
+
+    public static void removeItem(int index){
+        items.remove(index);
+        adapter.notifyDataSetChanged();
     }
 
     @Override
@@ -33,5 +61,11 @@ public class IngredientsActivity extends AppCompatActivity implements PopupMenu.
         popupMenu.setOnMenuItemClickListener(this);
         popupMenu.inflate(R.menu.popup_menu);
         popupMenu.show();
+        popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                return false;
+            }
+        });
     }
 }
