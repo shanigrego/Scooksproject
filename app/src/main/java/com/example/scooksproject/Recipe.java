4a5e1284e0b6850820e86640f742117a0503025f
shanigrego <79804876+shanigrego@users.noreply.github.com>
2022-04-10 00:25:43 +0300

    basic parse-done!
    basic parse to recipes

--- app/src/main/java/com/example/scooksproject/Recipe.java
+++ app/src/main/java/com/example/scooksproject/Recipe.java
@@ -1,10 +1,28 @@
 package com.example.scooksproject;
 
+import org.jsoup.select.Elements;
+
 import java.util.List;
 
 public class Recipe {
 
-    List<Ingredient> ingredients;
+    private String name;
+    private String timeOfWorkNeeded;
+    private String totalTimeRecipe;
+    private String difficultLevel;
+    private List<Ingredient> ingredients;
+    private Elements recipeInstructions;
+
+    public Recipe(String name, String timeOfWorkNeeded, String totalTimeRecipe, String difficultLevel,
+                    List<Ingredient> ingredients, Elements recipeInstructions) {
+        this.name=name;
+        this.timeOfWorkNeeded=timeOfWorkNeeded;
+        this.totalTimeRecipe=totalTimeRecipe;
+        this.difficultLevel=difficultLevel;
+        this.ingredients=ingredients;
+        this.recipeInstructions=recipeInstructions;
+    }
+
 
 
 }
